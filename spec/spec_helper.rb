98605b134c8311a624322247fb6969b1042d5e01
Aaron F Stanton <afstanton@gmail.com>
2025-10-08 12:47:56 -0400

    Rubocop etc

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,10 +1,16 @@
 # frozen_string_literal: true
 
-require "tool_forge"
+require 'simplecov'
+
+SimpleCov.start do
+  enable_coverage :branch
+end
+
+require 'tool_forge'
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
-  config.example_status_persistence_file_path = ".rspec_status"
+  config.example_status_persistence_file_path = '.rspec_status'
 
   # Disable RSpec exposing methods globally on `Module` and `main`
   config.disable_monkey_patching!
